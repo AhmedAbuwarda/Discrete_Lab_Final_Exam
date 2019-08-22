7975501216df85b8f38ee914971eefdd6711f6c9
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-22 14:24:34 +0300

    Update Question_03 file.

--- src/FinalExam/Question_03.java
+++ src/FinalExam/Question_03.java
@@ -18,9 +18,9 @@ class Question_03 {
     static void Q3() {
 
         // Create Sets R, A , B.
-        Set<Integer> R = new HashSet<Integer>();
-        Set<Integer> A = new HashSet<Integer>();
-        Set<Integer> B = new HashSet<Integer>();
+        Set<Integer> R = new HashSet<>();
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
 
         // Add Numbers To Set A.
         A.add(3);
@@ -38,19 +38,19 @@ class Question_03 {
         }
 
         // Create New Sets.
-        Set<Integer> AunionB = new HashSet<Integer>(A);
+        Set<Integer> AunionB = new HashSet<>(A);
         AunionB.addAll(B);
 
-        Set<Integer> removeUnion = new HashSet<Integer>(R);
+        Set<Integer> removeUnion = new HashSet<>(R);
         removeUnion.removeAll(AunionB);
 
-        Set<Integer> RremoveA = new HashSet<Integer>(R);
+        Set<Integer> RremoveA = new HashSet<>(R);
         RremoveA.removeAll(A);
 
-        Set<Integer> RremoveB = new HashSet<Integer>(R);
+        Set<Integer> RremoveB = new HashSet<>(R);
         RremoveB.removeAll(B);
 
-        Set<Integer> AinterB = new HashSet<Integer>(RremoveA);
+        Set<Integer> AinterB = new HashSet<>(RremoveA);
         AinterB.retainAll(RremoveB);
 
         // If Statement To Print The Result.
@@ -64,39 +64,39 @@ class Question_03 {
 
     }
 
-    /**
-     * Or you can use this code...
-     *
-     *         System.out.println("A = " + A + "B = " + B);
-     *         Set<Integer> AuB = new HashSet<>(A);
-     *         AuB.addAll(B);
-     *
-     *         System.out.println("AuB = " + AuB);
-     *         Set<Integer> AuBInvers = Invers(R, AuB);
-     *         System.out.println("AuBInvers = " + AuBInvers);
-     *
-     *         Set<Integer> AInvers = Invers(R, A);
-     *         System.out.println("AInvers = " + AInvers);
-     *
-     *         Set<Integer> BInvers = Invers(R, B);
-     *         System.out.println("BInvers = " + BInvers);
-     *
-     *         Set<Integer> AInvnBInv = new HashSet<>(AInvers);
-     *         AInvnBInv.retainAll(BInvers);
-     *         System.out.println("AInvnBInv = " + AInvnBInv);
-     *
-     *         System.out.println("End : " + AuBInvers + "   " + AInvnBInv);
-     *
-     *         if (AuBInvers.equals(AInvnBInv)) {
-     *             System.out.println("The formula is True (^_^)");
-     *         } else {
-     *             System.out.println("false");
-     *         }
-     *         public static Set<Integer> Invers(Set<Integer> A, Set<Integer> B) {
-     *              Set<Integer> Invers = new HashSet<>(A);
-     *              Invers.removeAll(B);
-     *              return Invers;
-     *         }
+    /*
+      Or you can use this code...
+
+              System.out.println("A = " + A + "B = " + B);
+              Set<Integer> AuB = new HashSet<>(A);
+              AuB.addAll(B);
+
+              System.out.println("AuB = " + AuB);
+              Set<Integer> AuBInvers = Invers(R, AuB);
+              System.out.println("AuBInvers = " + AuBInvers);
+
+              Set<Integer> AInvers = Invers(R, A);
+              System.out.println("AInvers = " + AInvers);
+
+              Set<Integer> BInvers = Invers(R, B);
+              System.out.println("BInvers = " + BInvers);
+
+              Set<Integer> AInvnBInv = new HashSet<>(AInvers);
+              AInvnBInv.retainAll(BInvers);
+              System.out.println("AInvnBInv = " + AInvnBInv);
+
+              System.out.println("End : " + AuBInvers + "   " + AInvnBInv);
+
+              if (AuBInvers.equals(AInvnBInv)) {
+                  System.out.println("The formula is True (^_^)");
+              } else {
+                  System.out.println("false");
+              }
+              public static Set<Integer> Invers(Set<Integer> A, Set<Integer> B) {
+                   Set<Integer> Invers = new HashSet<>(A);
+                   Invers.removeAll(B);
+                   return Invers;
+              }
      */
 
 }
